68cc9874a90282ada95b6e63566fff9eb6e1351c
a-korkin <a-korkin88@yandex.ru>
2025-03-25 09:27:50 +0300

    feat: downloading csv

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,28 @@
 use reqwest;
 use std::fs;
+use std::io::prelude::*;
+use std::path::Path;
 
 pub async fn say_hello() -> String {
     String::from("who wants to be a rich? me!")
 }
 
-pub async fn download(url: &str) {
-    // let _res = reqwest::get(url)
-    //     .await
-    //     .expect("failed to download file")
-    //     .text()
-    //     .await
-    //     .expect("failed to get csv");
-    let path = "downloads/iss_moex";
+pub async fn download(url: &str) -> std::io::Result<()> {
+    let res = reqwest::get(url)
+        .await
+        .expect("failed to download file")
+        .text()
+        .await
+        .expect("failed to get csv");
+
+    let path = "data/iss_moex";
     if !fs::exists(path).unwrap() {
-        fs::create_dir_all(path).expect("failed to create dir");
+        fs::create_dir_all(path)?;
     }
+    let file_name = "moex.csv";
+    let file_path = Path::new(path).join(file_name);
+    let mut file = fs::File::create(file_path)?;
+    file.write_all(res.as_bytes())?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use money_maker::download;
+// use money_maker::download;
+use chrono::prelude::*;
 use std::io;
 use tokio;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let url = "https://iss.moex.com/iss/engines/stock/markets/shares/securities/MOEX/candles.csv?from=2025-03-03&till=2025-03-03&interval=10";
-    download(url).await;
+    let security = "MOEX";
+    let date = chrono::Utc
+        .with_ymd_and_hms(2025, 3, 3, 0, 0, 0)
+        .unwrap()
+        .format("%Y-%m-%d");
+    let interval: u8 = 10;
+    let url = format!("https://iss.moex.com/iss/engines/stock/markets/shares/securities/{security}/candles.csv?from={date}&till={date}&interval={interval}");
+    println!("{url}");
+    // download(&url).await?;
     Ok(())
 }
