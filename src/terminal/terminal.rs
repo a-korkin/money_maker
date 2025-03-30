d76aa41ea40075959335db036a754e0e203c66de
a-korkin <a-korkin88@yandex.ru>
2025-03-30 18:52:07 +0300

    fix: candle draw

--- src/terminal/terminal.rs
+++ src/terminal/terminal.rs
@@ -5,8 +5,8 @@ use raylib::prelude::*;
 use sqlx::PgPool;
 
 const H: f32 = 480.0;
-const W: f32 = 640.0;
-const PAD: f32 = 20.0;
+const W: f32 = 896.0;
+const CANDLE_W: f32 = 10.0;
 
 pub async fn run_terminal(pool: &PgPool) {
     let date = NaiveDate::from_ymd_opt(2025, 3, 25)
@@ -14,7 +14,8 @@ pub async fn run_terminal(pool: &PgPool) {
         .and_hms_opt(0, 0, 0)
         .unwrap();
 
-    let candles = pg::get_candles(pool, "MOEX", date).await;
+    let mut candles = pg::get_candles(pool, "MOEX", date).await;
+    candles.sort_by(|a, b| a.begin.cmp(&b.begin));
 
     let mut min_date: chrono::NaiveDateTime = candles.first().unwrap().begin;
     let mut max_date: NaiveDateTime = date;
@@ -49,6 +50,7 @@ pub async fn run_terminal(pool: &PgPool) {
     let max_y = f32::ceil(max_high);
     let plot_pos_start = Vector2::new(40.0, 20.0);
     let plot_pos_end = Vector2::new(W - 20.0, H - 20.0);
+    let step_y = (plot_pos_end.y - plot_pos_start.y) / (max_y - min_y);
 
     let (mut rl, thread) = raylib::init()
         .size(W as i32, H as i32)
@@ -66,9 +68,15 @@ pub async fn run_terminal(pool: &PgPool) {
             &plot_pos_end,
             min_y,
             max_y,
+            step_y,
+            Period::Hour,
             &candles,
         );
     }
+}
+
+enum Period {
+    Hour,
 }
 
 fn draw_axis(
@@ -77,17 +85,18 @@ fn draw_axis(
     end_pos: &Vector2,
     min_y: f32,
     max_y: f32,
-    _candles: &Vec<Candle>,
+    step_y: f32,
+    period: Period,
+    candles: &Vec<Candle>,
 ) {
     // y-axis
     d.draw_line_v(
         start_pos,
-        Vector2::new(start_pos.x, end_pos.y),
+        Vector2::new(start_pos.x, end_pos.y + 1_f32),
         Color::BLACK,
     );
 
     let mut cur_y = start_pos.y;
-    let step_y = (end_pos.y - start_pos.y) / (max_y - min_y);
     let mut label_y = max_y;
     while cur_y <= end_pos.y {
         d.draw_line_v(
@@ -115,4 +124,66 @@ fn draw_axis(
 
     // x-axis
     d.draw_line_v(Vector2::new(start_pos.x, end_pos.y), end_pos, Color::BLACK);
+
+    let center = (end_pos.x - start_pos.x) / 2.0;
+    let half = f32::ceil(candles.len() as f32 / 2.0);
+    let first_indx_pos: f32 = center - (half * CANDLE_W);
+
+    let mut right = center;
+    let mut left = center;
+    let mut i = 0;
+    while right <= end_pos.x {
+        let scale = if i % 5 == 0 { 5_f32 } else { 3_f32 };
+        d.draw_line_v(
+            Vector2::new(right, end_pos.y + scale),
+            Vector2::new(right, end_pos.y - scale),
+            Color::BLACK,
+        );
+        if left >= start_pos.x {
+            d.draw_line_v(
+                Vector2::new(left, end_pos.y + scale),
+                Vector2::new(left, end_pos.y - scale),
+                Color::BLACK,
+            );
+        }
+
+        right += CANDLE_W;
+        left -= CANDLE_W;
+        i += 1;
+    }
+
+    draw_candle(d, &candles[6], first_indx_pos, start_pos, step_y, max_y);
+}
+
+fn convert_coords(start_pos: Vector2, step_y: f32, max_y: f32, in_value_y: f32) -> f32 {
+    (max_y - in_value_y) * step_y + start_pos.y
+}
+
+fn draw_candle(
+    d: &mut RaylibDrawHandle,
+    candle: &Candle,
+    idx_pos: f32,
+    start_pos: &Vector2,
+    step_y: f32,
+    max_y: f32,
+) {
+    let max = f32::max(candle.close, candle.open);
+    let min = f32::min(candle.close, candle.open);
+    let color = if candle.close > candle.open {
+        Color::GREEN
+    } else {
+        Color::RED
+    };
+    let pos = Vector2::new(idx_pos, convert_coords(*start_pos, step_y, max_y, max));
+    let size = Vector2::new(CANDLE_W, (max - min) * step_y);
+    d.draw_rectangle_v(pos, size, color);
+    let high = Vector2::new(
+        idx_pos + CANDLE_W / 2.0,
+        convert_coords(*start_pos, step_y, max_y, candle.high),
+    );
+    let low = Vector2::new(
+        idx_pos + CANDLE_W / 2.0,
+        convert_coords(*start_pos, step_y, max_y, candle.low),
+    );
+    d.draw_line_v(high, low, color);
 }
