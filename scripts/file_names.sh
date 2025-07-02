67e1f79d49e0af3a688804f19dfefd8a3e49287a
a-korkin <a-korkin88@yandex.ru>
2025-07-02 06:26:36 +0300

    fix: renaming script

--- scripts/file_names.sh
+++ scripts/file_names.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 source .env
-DIR_CANDLES=$DATA_DIR/candles
-# DIR_CANDLES=data/candles
     
 rename 's/([0-9]{4}-[0-9]{2}-[0-9]{2}_)([0-9]{1})(\.csv)/${1}0${2}${3}/' \
-    $DIR_CANDLES/**/*.csv
+    $DATA_DIR/candles/**/*.csv
